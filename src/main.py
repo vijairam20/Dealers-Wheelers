2d4c83a71769bf64d6d07f28a337cf8b9c59a474
vijairam20 <vijairam20@gmail.com>
2018-02-17 02:22:14 +0300

    Update Purchase and buy functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import os
 import sys
 import csv
+import time
 import pickle
 from decimal import Decimal
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+CARS = os.path.join(THIS_FOLDER, 'mtcars.csv')
+USERS = os.path.join(THIS_FOLDER,'user.csv')
 class User:
     def __init__(self,name , username ,password , cars = 0):
         self.name = name
@@ -68,7 +72,7 @@ def removeDuplicates(types) :
     return Ntypes
 def getDetails(n , spec ="" , a = 0):
     types = []
-    file = open("src/mtcars.csv","r",newline='')
+    file = open(CARS, "r", newline='')
     if spec == "" :
         with file :
             reader = csv.reader(file)
@@ -95,7 +99,7 @@ def init():
         while  u == None :
             u = authenticate()
         os.system("cls")
-        purchase(u)
+        action(u)
     elif(flag == 2):
         os.system("cls")
         obj = create_user()
@@ -116,7 +120,7 @@ def create_user():
     print("Enter Password : ")
     password = input()
     obj = User(name, username, password)
-    file = open('user.csv','a',newline='')
+    file = open(USERS,'a',newline='')
     data =[[name,username,password]]
     with file:
         writer = csv.writer(file)
@@ -128,7 +132,7 @@ def authenticate():
     username = input()
     print("Enter Password :")
     password = input()
-    file = open('user.csv','r',newline='')
+    file = open(USERS,'r',newline='')
     flag = False
     with file:
         reader = csv.reader(file)
@@ -145,7 +149,7 @@ def authenticate():
             return
     return u
 #Purchase
-def purchase(u):
+def action(u):
     printHeader()
     print("Welcome back "+u.name)
     print("1.Buy")
@@ -154,14 +158,8 @@ def purchase(u):
     print("3.Quit")
     flag =(int)(input())
     if flag == 1 :
-        print("Enter prefered Manufacturer : ")
-        man = getManufacturers()
-        print("Enter preferred Type :")
-        Type = getType(man)
-        cars = getCar(man,Type)
-        for c in cars :
-            c.printDetails()
-
+        os.system("cls")
+        buy()
     elif flag == 3 :
         print("Details")
         u.printDetails()
@@ -170,7 +168,7 @@ def purchase(u):
     else :
         init()
 def getCars():
-    file = open("src/mtcars.csv",'r',newline='')
+    file = open(CARS, 'r', newline='')
     cars = []
     with file:
         reader = csv.reader(file)
@@ -205,14 +203,135 @@ def getType(man ="") :
     print("Press NUM for type")
     inp = int(input())
     return types[inp-1]
-def getCar(man , type) :
-    file = open("src/mtcars.csv",'r',newline='')
+def getCar(man = "", type ="") :
+    file = open(CARS, 'r', newline='')
     cars =[]
+    flag_1 = True
+    flag_2 = True
+    if man == "" :
+        flag_1 = False
+    if type == "":
+        flag_2 =False
     with file :
         reader = csv.reader(file)
         for row in reader :
-            if row[0] == man and row[2] == type :
-                cars.append(Car(row[0],row[1],row[2],Decimal(row[3])+Decimal(row[4]),int(row[5]),int(row[6]),Decimal(row[7]),Decimal(row[8]),Decimal(row[9]),Decimal(row[10]),Decimal(row[12]),Decimal(row[13]),int(row[14])))
+            if flag_1 and flag_2 :
+                if row[0] == man and row[2] == type :
+                    cars.append(Car(row[0],row[1],row[2],Decimal(row[3])+Decimal(row[4]),int(row[5]),int(row[6]),Decimal(row[7]),Decimal(row[8]),Decimal(row[9]),Decimal(row[10]),Decimal(row[12]),Decimal(row[13]),int(row[14])))
+            elif flag_1 :
+                if row[0] == man :
+                    cars.append(Car(row[0],row[1],row[2],Decimal(row[3])+Decimal(row[4]),int(row[5]),int(row[6]),Decimal(row[7]),Decimal(row[8]),Decimal(row[9]),Decimal(row[10]),Decimal(row[12]),Decimal(row[13]),int(row[14])))
+            elif flag_2 :
+                if row[2] == type :
+                    cars.append(Car(row[0],row[1],row[2],Decimal(row[3])+Decimal(row[4]),int(row[5]),int(row[6]),Decimal(row[7]),Decimal(row[8]),Decimal(row[9]),Decimal(row[10]),Decimal(row[12]),Decimal(row[13]),int(row[14])))
+    return cars
+def buy():
+    printHeader()
+    print("Enter Option")
+    print("1.Search parameters :")
+    print("2.Sort By parameters :")
+    flag = int(input())
+    if flag == 1 :
+        os.system("cls")
+        printHeader()
+        print("1.Manufacturer")
+        print("2.Type")
+        print("3.Both")
+        manuf = ""
+        Type = ""
+        flag = int(input())
+        if flag == 1:
+            manuf = getManufacturers()
+        elif flag == 2:
+            Type = getType()
+        elif flag ==3 :
+            manuf = getManufacturers()
+            Type  = getType(manuf)
+        cars = getCar(manuf,Type)
+        Bcar = getBuyCar(cars)
+        os.system("cls")
+        print("Bought")
+        Bcar.printDetails()
+    if flag == 2 :
+        os.system("cls")
+        printHeader()
+        print("Sort By")
+        print("1.Price")
+        print("2.Mileage")
+        print("3.Horse Power")
+        print("4.Fueltank")
+        flag = int(input())
+        params = ["price","mileage","horse power" ,"fueltank"]
+        symbols =["$","L/100km","HP","L"]
+        if flag :
+            printHeader()
+            os.system("cls")
+            print("By "+params[flag-1])
+            cars = getCars()
+            cars = sortBy(cars,flag) #Remember to add sort for all parameters
+            print("Select:")
+            print("1.Ascending")
+            print("2.Descending")
+            fl = (int)(input())
+            if fl == 2 :
+                cars.reverse()
+            i = 1
+            for c in cars :
+                if flag == 1 :
+                    print(str(i)+":"+c.manuf+"-"+c.model+" $"+str(c.price))
+                elif flag == 2 :
+                    print(str(i)+":"+c.manuf+"-"+c.model+" "+str(c.mpg)+"L/100km")
+                elif flag == 3 :
+                    print(str(i)+":"+c.manuf+"-"+c.model+" "+str(c.hp)+"HP")
+                elif flag == 4 :
+                    print(str(i)+":"+c.manuf+"-"+c.model+" "+str(c.ft)+"L")
+                i+=1
+def getBuyCar(cars) :
+    i = 0
+    inp = 0
+    c = 1
+    while inp == 0:
+        while i < 5 and c-1 < len(cars):
+            print(str(c) + ":" + cars[c - 1].manuf+"-"+cars[c-1].model)
+            c += 1
+            i += 1
+        i = 0
+        print("Press NUM for Car ")
+        print("Press 0 for more")
+        inp = int(input())
+    return cars[inp-1]
+def sortBy(cars,param) :
+    size = len(cars)
+    t = param
+    minI = 0
+    if param == 1 :
+        for i in range(0, size) :
+            d = i + 1
+            for d in range(d, size) :
+                if cars[d].price < cars[minI].price:
+                    minI = d
+            cars[i], cars[minI] = cars[minI], cars[i]  #  Swapping Two variables
+    elif param == 2 :
+        for i in range(0, size) :
+            d = i + 1
+            for d in range(d, size) :
+                if cars[d].mpg < cars[minI].mpg:
+                    minI = d
+            cars[i], cars[minI] = cars[minI], cars[i]  #  Swapping Two variables
+    elif t == 3 :
+        for i in range(0, size) :
+            d = i + 1
+            for d in range(d, size) :
+                if cars[d].hp < cars[minI].hp:
+                    minI = d
+            cars[i], cars[minI] = cars[minI], cars[i]  #  Swapping Two variables
+    elif t == 4 :
+        for i in range(0, size) :
+            d = i + 1
+            for d in range(d, size) :
+                if cars[d].ft < cars[minI].ft:
+                    minI = d
+            cars[i], cars[minI] = cars[minI], cars[i]  #  Swapping Two variables
     return cars
 #main
 init()
